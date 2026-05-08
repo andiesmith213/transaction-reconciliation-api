9504d35b290d4f1770913d039820f931563dee5d
andiesmith213 <andie.smith213@gmail.com>
2026-05-08 11:09:22 -0700

    Added tests for error checking

--- tests/unit/test_matcher.py
+++ tests/unit/test_matcher.py
@@ -2,6 +2,8 @@
 
 from app.core.match_transaction import match_events
 
+import pytest
+
 def test_valid_transaction_match(pending_tx_factory, matching_tx_factory, 
                                  pending_storage, matching_storage):
     #Setup:
@@ -36,3 +38,51 @@ def test_valid_transaction_match(pending_tx_factory, matching_tx_factory,
     #Check match storage against expected match
     assert matching_storage.tx[42] == matched_tx1
     assert matching_storage.tx[35] == matched_tx2
+    #Check pending storage does not contain transaction
+    with pytest.raises(KeyError):
+        pending_storage.internal_tx[42]
+        pending_storage.internal_tx[35]
+        pending_storage.processed_tx[42]
+        pending_storage.processed_tx[35]
+
+def test_missing_processed_tx(pending_tx_factory,pending_storage, 
+                              matching_storage):
+    #Setup:
+    #Create pending transactions and store in pending storage
+    pending_tx1 = pending_tx_factory(tx_id = 42, amount = 115)  
+    pending_storage.set_internal_transaction(pending_tx1)
+
+    #Execution:
+    #Call match_events function
+    match = match_events(42)
+    assert match["status"] == "waiting for processed"
+    #Check matched storage does not contain transaction
+    with pytest.raises(KeyError):
+        matching_storage.tx[42]
+    #Check that internal transaction was placed back in pending storage
+    assert pending_storage.get_internal_list() == [pending_tx1]
+
+def test_missing_internal_tx(pending_tx_factory,pending_storage, 
+                             matching_storage):
+    #Setup:
+    #Create pending transactions and store in pending storage
+    pending_tx1 = pending_tx_factory(tx_id = 42, amount = 115)  
+    pending_storage.set_processed_transaction(pending_tx1)
+
+    #Execution:
+    #Call match_events function
+    match = match_events(42)
+    assert match["status"] == "waiting for internal"
+    #Check matched storage does not contain transaction
+    with pytest.raises(KeyError):
+        matching_storage.tx[42]
+    #Check that internal transaction was placed back in pending storage
+    assert pending_storage.get_processed_list() == [pending_tx1]
+
+def test_both_tx_missing(matching_storage):
+    #Call match_events function
+    match = match_events(42)
+    assert match["status"] == "transactions not found"
+    #Check matched storage does not contain transaction
+    with pytest.raises(KeyError):
+        matching_storage.tx[42]
